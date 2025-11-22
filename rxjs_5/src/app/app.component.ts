71c83b372ff1211f6938af1c41ea03d1b37dfa03
Valentin Wissler <valentin.wissler@n-able.com>
2025-11-22 16:06:40 +0000

    challenge 5 complete

--- rxjs_5/src/app/app.component.ts
+++ rxjs_5/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Subject } from "rxjs/internal/Subject";
+import { switchMap } from "rxjs/internal/operators/switchMap";
+import { take } from "rxjs/internal/operators/take";
+import { interval } from "rxjs/internal/observable/interval";
+import { map } from "rxjs/internal/operators/map";
+import { endWith } from "rxjs/internal/operators/endWith";
 
 @Component({
   selector: "my-app",
@@ -9,5 +15,15 @@ import { CommonModule } from "@angular/common";
   imports: [CommonModule],
 })
 export class AppComponent {
-  
+  protected trigger$ = new Subject<void>();
+
+  protected count$ = this.trigger$.pipe(
+    switchMap(() => {
+      return interval(1000).pipe(
+        take(6),
+        map(v => 5 - v ),
+        endWith("Time's up!")
+      );
+    })
+  ); 
 }
---
+++ rxjs_5/src_base/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class AppComponent {
+  
+}
